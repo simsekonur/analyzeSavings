22f4288ca125fb5bf182f62601996d8e609e6d27
onur <onur@onurs-MacBook-Pro.local>
2021-12-29 21:05:20 +0300

    Create a database connection and wake up the backend server

--- api/server.js
+++ api/server.js
@@ -1,2 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+dotenv.config();
+// connect to the database with connection string
+mongoose.connect(process.env.URL, {useNewUrlParser: true});
+
+// take the connection from mongoose
+const db = mongoose.connection;
+
+db.once('open', () => console.log('Connected to database'));
+
+app.use(express.json());
+app.listen(PORT, () => console.log('Server is listening!'));
