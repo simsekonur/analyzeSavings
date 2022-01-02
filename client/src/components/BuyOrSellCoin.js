1fae0d3edf089566a34e2550c161b958d21f656a
onur <onur@onurs-MacBook-Pro.local>
2022-01-03 00:47:43 +0300

    Send necessary api calls to backend

--- client/src/components/BuyOrSellCoin.js
+++ client/src/components/BuyOrSellCoin.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText, Container, Row, Col } from 'reactstrap';
+import React, { useCallback, useState } from 'react';
+import { Button, Form, FormGroup, Label, Input, Container, Row, Col } from 'reactstrap';
 
+import axios from 'axios';
 const mainStyle = {
     border: '2px solid #d3d3d3', 
     marginLeft: 'auto',
@@ -14,17 +15,55 @@ const mainStyle = {
 }
 
 const BuyOrSellCoin = () => {
+
+    // component based states
+    const [isBuy, setBuy] = useState('Buy');    
+    const [name, setName] = useState('');
+    const [nameChecked, setNameChecked] = useState(true);    
+    const [quantity, setQuantity] = useState(0);
+    const [bnbPrice, setBnbPrice] = useState(0);    
+
+    const [commission, setCommission] = useState(0);
+    const [unitCost, setUnitCost] = useState(0)    
+
+    const postCoin = useCallback(async (data) => {
+        await axios.post('http://localhost:3001/records', data);
+    }, []);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({isBuy, name, quantity, bnbPrice, unitCost, commission});
+        const isBought = isBuy === 'Buy' ? true : false;
+
+        const totalCommission = bnbPrice * commission;
+        console.log({ isBought, totalCommission});
+
+        if (name === '' || quantity === 0 || unitCost === 0) {
+            alert('Write all of them!');
+            return 
+        }
+
+        // do some conversion 
+        const quantityFloat = parseFloat(quantity);
+        const unitCostFloat = parseFloat(unitCost);
+        // send a request to the api end point
+        const response = postCoin({isBought, name, quantity: quantityFloat, unitCost: unitCostFloat, totalCommission});
+        console.log(response);
+        // clear some fields up
+        !nameChecked && setName(''); 
+
+    }
     return (
         <div style={{ textAlign: 'center'}}>
-            <h1 style={{ color: 'red'}}> Add or sell the coin</h1>
+            <h1 style={{ color: 'red'}}> Buy or sell the coin</h1>
             <div style={mainStyle}>
                 <Container>
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <Row>
                             <Col>
                                 <FormGroup>
                                     <Label for="exampleSelect">Buy or Sell</Label>
-                                    <Input type="select" name="select" id="exampleSelect">
+                                    <Input type="select" name="buyOrSell" value={isBuy} onChange={(e) => { console.log(e.target.value); setBuy(e.target.value)}}>
                                         <option>Buy</option>
                                         <option>Sell</option>
                                     </Input>
@@ -33,38 +72,55 @@ const BuyOrSellCoin = () => {
                             <Col>
                                 <FormGroup>
                                     <Label for="exampleEmail">Current Bnb Price</Label>
-                                    <Input type="number" step="0.01" name="bnbPrice" placeholder="Current bnb price" />
-                                </FormGroup>
-                            </Col>
-                        </Row>
-
-                        <Row>
-                            <Col>
-                                <FormGroup>
-                                    <Label for="exampleEmail">Name</Label>
-                                    <Input type="text" name="name" placeholder="Name" />
-                                </FormGroup>
-                            </Col>
-                            <Col>
-                                <FormGroup>
-                                    <Label for="exampleEmail">Unit Cost</Label>
-                                    <Input type="number" step= "0.1" name="unitCost" placeholder="Unit cost" />
-                                </FormGroup>
-                            </Col>
-                            <Col>
-                                <FormGroup>
-                                    <Label for="exampleEmail">Quantity</Label>
-                                    <Input type="number" step= "0.1" name="quantity" placeholder="Quantity" />
+                                    <Input type="number"
+                                    value={bnbPrice}
+                                    onChange={(e) => setBnbPrice(e.target.value)}
+                                    step="0.01"
+                                    name="bnbPrice"
+                                    placeholder="Current bnb price" />
                                 </FormGroup>
                             </Col>
                         </Row>
 
                         <FormGroup>
-                            <Label for="exampleEmail">Commission</Label>
-                            <Input type="number" step="0.00001" name="bnbPrice" placeholder="Commission" />
+                            <Label for="exampleEmail">Name</Label>
+                            <Input type="text"
+                                name="name" 
+                                placeholder="Name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                />
                         </FormGroup>
-
-                        <Button>Submit</Button>
+                        <Row>
+                            <Col>
+                                <FormGroup>
+                                    <Label for="exampleEmail">Unit Cost</Label>
+                                    <Input type="number" step= "0.1" name="unitCost" value= {unitCost} 
+                                    onChange={(e) => setUnitCost(e.target.value) } placeholder="Unit cost" />
+                                </FormGroup>
+                            </Col>
+                            <Col>
+                                <FormGroup>
+                                    <Label for="exampleEmail">Quantity</Label>
+                                    <Input type="number" step= "0.1" name="quantity" placeholder="Quantity"
+                                    value={quantity} onChange={(e) => setQuantity(e.target.value)} />
+                                </FormGroup>
+                            </Col>
+                            <Col>
+                            <FormGroup>
+                                <Label for="exampleEmail">Commission</Label>
+                                <Input type="number" step="0.00001" name="bnbPrice" placeholder="Commission"
+                                value={ commission} onChange={(e) => setCommission(e.target.value)}/>
+                            </FormGroup>
+                            </Col>
+                        </Row>
+                        <FormGroup>
+                            <Input type="checkbox" name="nameChecked" checked={nameChecked}
+                            value={ nameChecked} onChange={(e) => setNameChecked(!nameChecked)}/>
+                            <Label>Name will be the same</Label>
+                        </FormGroup>
+                        
+                        <Button type='submit'> Submit </Button>
                     </Form>
                 </Container>
             </div>
--- client/src/components/ListCoins.js
+++ client/src/components/ListCoins.js
@@ -4,8 +4,6 @@ import { Table, Input, Button } from 'reactstrap';
 import axios from "axios";
 const ListCoins = () => {
     const [coins, setCoins] = useState([]);
-    let [totalMoneyAllocated, setTotalMoneyAllocated] = useState(0);
-    let [totalMoney, setTotalMoney] = useState(0);
     let [currentPrice, setCurrentPrice] = useState(0);
 
     const fetchCoins = useCallback(async () => {
@@ -13,19 +11,19 @@ const ListCoins = () => {
         .then((response) => {
             // console.log(response);
             setCoins(response.data);
-            response.data.forEach((item) => {
-                totalMoneyAllocated += item.totalPrice;
-                totalMoney += item.currentTotalPrice;
-            })
         })
 
         // console.log(coins);
     }, []);
+
+    const updateCoin = useCallback(async (id) => {
+        await axios.post(`http://localhost:3001/records/${id}`, { currentPrice });
+        fetchCoins();
+    }, [currentPrice, fetchCoins]);
     
     useEffect(() => {
         fetchCoins();
     }, [fetchCoins])
-
     
     return (
         <div>
@@ -40,29 +38,36 @@ const ListCoins = () => {
                     <th>Total Allocated</th>
                     <th>Total Price</th>
                     <th>Total Commision</th>
+                    <th>Update</th>
                 </tr>
                 </thead>
                 <tbody>
                 {coins.map((coin, index) => {
                     return (
-                        <tr>
+                        <tr key={index}>
                             <th scope="row">{index +1 }</th>
                             <td>{coin.name}</td>
                             <td>{coin.unitCost}</td>
                             <td>{coin.quantity}</td>
                             <td>
-                                <Input type="number" step= "0.1" name="currentPrice" placeholder="Price" onChange={(e) => setCurrentPrice(e.target.value)}/>
+                                <Input type="number" 
+                                    value={currentPrice} 
+                                    step= "0.00001" 
+                                    name="currentPrice" 
+                                    placeholder="Price" 
+                                    onChange={(e) => setCurrentPrice(e.target.value)}
+                                />
                             </td>
+                            <td>{Math.round(coin.unitCost * coin.quantity*100) / 100}</td>
                             <td>{coin.totalPrice}</td>
-                            <td>{coin.totalCommission}</td>
-                            <td>{coin.currentTotalPrice}</td>
+                            <td>{Math.round(coin.totalCommission * 100) / 100}</td>
+                            <td> <Button onClick={() => updateCoin(coin._id)}> Save </Button> </td>
                         </tr>
 
                     );
                 })}
                 </tbody>
             </Table>
-            <Button> Submit to Database</Button>
         </div>
     )
 }
